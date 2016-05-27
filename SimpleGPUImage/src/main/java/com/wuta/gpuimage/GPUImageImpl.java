a230bceae529aca90f371a516a55f82e9b2109ec
seabreathe <liweihfyz@hotmail.com>
2016-05-27 16:08:01 +0800

    facedector function adder time-comsuming

--- SimpleGPUImage/src/main/java/com/wuta/gpuimage/GPUImageImpl.java
+++ SimpleGPUImage/src/main/java/com/wuta/gpuimage/GPUImageImpl.java
@@ -358,9 +358,9 @@ public class GPUImageImpl implements IGPUImage
                     adjustImageScaling();
                 }
             });
-        }/*
-        if(num(20))
-        {
+        }
+        if(num(20)) {
+            /*
             File file = new File("/storage/emulated/0/liwei");
             File file2 = new File("/storage/emulated/0/liwei/1.jpg");
             file.mkdirs();
@@ -370,14 +370,37 @@ public class GPUImageImpl implements IGPUImage
             {
                 e.printStackTrace();
             }
-            YuvImage image = new YuvImage(data, ImageFormat.NV21,1920,1080,null);
+
+            //YuvImage image = new YuvImage(data, ImageFormat.NV21,1920,1080,null);
+
             try{
-                image.compressToJpeg(new Rect(0,0,1920,1080),50,new FileOutputStream(file2));
+                //image.compressToJpeg(new Rect(0,0,1920,1080),50,new FileOutputStream(file2));
+                bitmap.compress(Bitmap.CompressFormat.JPEG,50,new FileOutputStream(file2));
             }catch(IOException e)
             {
 
             }
-        }*/
+            */
+            long starttime = System.nanoTime();
+            bitmap = rawByteArray2RGBABitmap2(data, 1920, 1080);
+            Bitmap bm = bitmap.copy(Bitmap.Config.RGB_565, true);
+            FD = new FaceDetector(bm.getWidth(), bm.getHeight(), 2);
+            faces = new FaceDetector.Face[2];
+            long endtime = System.nanoTime();
+            Log.e("time for prepare",""+(endtime-starttime));
+            long starttime2 = System.nanoTime();
+            int realnum = FD.findFaces(bm, faces);
+            long endtime2 = System.nanoTime();
+            Log.e("time for prepare",""+(endtime2-starttime2));
+            if (realnum > 0) {
+                faces[0].getMidPoint(midpoint);
+                Log.e("detected faces", " " + midpoint.x + "  y is  " + midpoint.y);
+            }
+            else
+                Log.e("detected results:", "no faces detected");
+        }
+
+
 
         if (mRunOnDraw.isEmpty()) {
             runOnDraw(new Runnable() {
@@ -391,9 +414,27 @@ public class GPUImageImpl implements IGPUImage
         else {
             camera.addCallbackBuffer(data);
         }
-
-
-
+    }
+    public Bitmap rawByteArray2RGBABitmap2(byte[] data, int width, int height) {
+        int frameSize = width * height;
+        int[] rgba = new int[frameSize];
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++) {
+                int y = (0xff & ((int) data[i * width + j]));
+                int u = (0xff & ((int) data[frameSize + (i >> 1) * width + (j & ~1) + 0]));
+                int v = (0xff & ((int) data[frameSize + (i >> 1) * width + (j & ~1) + 1]));
+                y = y < 16 ? 16 : y;
+                int r = Math.round(1.164f * (y - 16) + 1.596f * (v - 128));
+                int g = Math.round(1.164f * (y - 16) - 0.813f * (v - 128) - 0.391f * (u - 128));
+                int b = Math.round(1.164f * (y - 16) + 2.018f * (u - 128));
+                r = r < 0 ? 0 : (r > 255 ? 255 : r);
+                g = g < 0 ? 0 : (g > 255 ? 255 : g);
+                b = b < 0 ? 0 : (b > 255 ? 255 : b);
+                rgba[i * width + j] = 0xff000000 + (b << 16) + (g << 8) + r;
+            }
+        Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        bmp.setPixels(rgba, 0 , width, 0, 0, width, height);
+        return bmp;
     }
     static int tmp =0;
     public boolean num(int i)
